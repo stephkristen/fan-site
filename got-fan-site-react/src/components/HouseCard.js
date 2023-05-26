7a58fa37ca2fbc75219936225d3acd2f5e048cdb
Anthony Rocco-Frank <106894159+AnthonyRoccoFrank@users.noreply.github.com>
2023-05-26 10:30:39 -0500

    Houses Done

--- got-fan-site-react/src/components/HouseCard.js
+++ got-fan-site-react/src/components/HouseCard.js
@@ -1,70 +1,32 @@
-import { useState, useEffect } from 'react';
-
+import React from "react";
 
 function HouseCard({ house }) {
-  const [swornMembers, setSwornMembers] = useState([]);
-  const [seats, setSeats] = useState([]);
-
-  useEffect(() => {
-    const fetchSwornMembers = async () => {
-      const memberPromises = house.swornMembers.map(async (member) => {
-        const response = await fetch(member);
-        const data = await response.json();
-        return data.name;
-      });
-      const members = await Promise.all(memberPromises);
-      setSwornMembers(members);
-    };
-
-    const fetchSeats = async () => {
-      const seatPromises = house.seats.map(async (seat) => {
-        const response = await fetch(seat);
-        const data = await response.json();
-        return data.name;
-      });
-      const seatNames = await Promise.all(seatPromises);
-      setSeats(seatNames);
-    };
-
-    fetchSwornMembers();
-    if (house.seats && house.seats.length > 0) {
-      fetchSeats();
-    } else {
-      setSeats([]);
-    }
-  }, [house.swornMembers, house.seats]);
-
   return (
     <div className="col">
-      <h5 className="card-header text-white">{house.name}</h5>
-      <div className="card-body text-white">
-        <ul>
-          <li>Region: {house.region}</li>
-          <li>
-            Seats:{" "}
-            {seats.length > 0 ? (
-              <ul>
-                {seats.map((seat, index) => (
-                  <li key={index}>{seat}</li>
-                ))}
-              </ul>
-            ) : (
-              "N/A"
-            )}
-          </li>
-          <li>
-            Sworn Members:{" "}
-            {swornMembers.length > 0 ? (
-              <ul>
-                {swornMembers.map((member, index) => (
-                  <li key={index}>{member}</li>
-                ))}
-              </ul>
-            ) : (
-              "N/A"
-            )}
-          </li>
-        </ul>
+      <div className="card text-white bg-dark mb-3">
+        <div className="card-header text-center">{house.name}</div>
+        <div className="card-body d-flex flex-column align-items-center">
+          <img
+            src={house.coatOfArms}
+            alt={house.name}
+            style={{ width: "100px", height: "100px" }}
+            className="mb-3"
+          />
+          <ul className="list-unstyled text-center">
+            <li>
+              <strong>Region:</strong> {house.region}
+            </li>
+            <li>
+              <strong>Sigil:</strong> {house.sigil}
+            </li>
+            <li>
+              <strong>Words:</strong> {house.words}
+            </li>
+            <li>
+              <strong>Seat:</strong> {house.seat}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- got-fan-site-react/src/components/Houses.js
+++ got-fan-site-react/src/components/Houses.js
@@ -1,55 +1,106 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import HouseCard from "./HouseCard";
 import backgroundImg from "../images/background-1.jpg";
+import houseStarkImg from "../images/House_Stark.jpg";
+import houseLannisterImg from "../images/House_Lannister.png"
+import houseTargaryenImg from "../images/House_Targaryen.png"
+import houseBaratheonImg from "../images/House_Baratheon.png"
+import houseTyrellImg from "../images/House_Tyrell.png"
+import houseGreyjoyImg from "../images/House_Greyjoy.png"
+import houseMartellImg from "../images/House_Martell.png"
+import houseArrynImg from "../images/House_Arryn.png"
+import houseTullyImg from "../images/House_Tully.png"
 
-export default function Houses() {
-    const [houses, setHouses] = useState([]);
-    const navigate = useNavigate();
-
-    async function getHouses() {
-        try {
-            const response = await fetch('https://www.anapioficeandfire.com/api/houses/');
-            if (response.status === 200) {
-                const json = await response.json();
-                setHouses(json);
-            } else {
-                navigate('/error');
-            }
-        } catch (error) {
-            console.log(error);
-        }
+const houses = [
+    {
+      name: "House Stark",
+      region: "The North",
+      sigil: "Direwolf",
+      words: "Winter Is Coming",
+      seat: "Winterfell",
+      coatOfArms: houseStarkImg
+    },
+    {
+      name: "House Lannister",
+      region: "The Westerlands",
+      sigil: "Lion",
+      words: "Hear Me Roar!",
+      seat: "Casterly Rock",
+      coatOfArms: houseLannisterImg
+    },
+    {
+      name: "House Targaryen",
+      region: "The Crownlands",
+      sigil: "Three-headed Dragon",
+      words: "Fire and Blood",
+      seat: "Dragonstone",
+      coatOfArms: houseTargaryenImg
+    },
+    {
+      name: "House Baratheon",
+      region: "The Stormlands",
+      sigil: "Stag",
+      words: "Ours Is the Fury",
+      seat: "Storm's End",
+      coatOfArms: houseBaratheonImg
+    },
+    {
+      name: "House Tyrell",
+      region: "The Reach",
+      sigil: "Rose",
+      words: "Growing Strong",
+      seat: "Highgarden",
+      coatOfArms: houseTyrellImg
+    },
+    {
+      name: "House Greyjoy",
+      region: "The Iron Islands",
+      sigil: "Kraken",
+      words: "We Do Not Sow",
+      seat: "Pyke",
+      coatOfArms: houseGreyjoyImg
+    },
+    {
+      name: "House Martell",
+      region: "Dorne",
+      sigil: "Sun and Spear",
+      words: "Unbowed, Unbent, Unbroken",
+      seat: "Sunspear",
+      coatOfArms: houseMartellImg
+    },
+    {
+      name: "House Arryn",
+      region: "The Vale of Arryn",
+      sigil: "Falcon and Crescent Moon",
+      words: "As High as Honor",
+      seat: "The Eyrie",
+      coatOfArms: houseArrynImg
+    },
+    {
+      name: "House Tully",
+      region: "The Riverlands",
+      sigil: "Trout",
+      words: "Family, Duty, Honor",
+      seat: "Riverrun",
+      coatOfArms: houseTullyImg
     }
+  ];
 
-    useEffect(() => {
-        getHouses();
-    }, []);
-
-    if (!houses.length) {
-        return (
-            <div 
-                className="d-flex justify-content-center"
-                style={{ height: '90vh', alignItems: 'center' }}>
-                <div className='spinner-border' role='status'>
-                    <span className='visually-hidden'>Loading...</span>
-                </div>
-            </div>
-        );
-    }
-
+  export default function Houses() {
     return (
-        <div
-          className="page-container"
-          style={{ backgroundImage: `url(${backgroundImg})` }} // Set the page background image
-        >
-          <h1 className="text-center mt-4 mb-4 text-white">Game of Thrones Houses</h1> {/* Set the heading text color to white */}
-          <div>
-            <div className="row row-cols-1 row-cols-md-3 g-4">
-              {houses.map((house) => {
-                return <HouseCard house={house} />;
-              })}
-            </div>
+      <div
+        className="page-container"
+        style={{ backgroundImage: `url(${backgroundImg})` }}
+      >
+        <h1 className="text-center mt-4 mb-4 text-white">
+          Game of Thrones Major Houses
+        </h1>
+        <div>
+          <div className="row row-cols-1 row-cols-md-3 g-4">
+            {houses.map((house, index) => (
+              <HouseCard key={index} house={house} />
+            ))}
           </div>
         </div>
-      );
-}
+      </div>
+    );
+  }
